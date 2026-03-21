84ef7b3724fdfdbfa74518a2ff513e1f1434098c
seomunji <162777421+SEOMUNJI@users.noreply.github.com>
2026-03-21 21:45:00 +0900

    [백준] 10818번 최소, 최대 - Python

---
+++ class1/10818.py
@@ -0,0 +1,12 @@
+# N = int(input())
+
+# nums = list(map(int, input().split()))
+
+# print(min(nums), max(nums))
+
+N = int(input())
+
+nums = map(int, input().split())
+
+print(min(nums))
+print(max(nums))
